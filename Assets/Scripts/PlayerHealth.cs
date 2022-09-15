64b939e17103162890c8263f5eeb52df9511c709
Ege Oral <egeoral@gmail.com>
2022-09-15 22:49:55 +0300

    Zombie attack animation adjusted.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] float playerHitPoint = 100f;
+    [SerializeField] TextMeshProUGUI playerHealthText;
+
+    private void Update() 
+    {
+        playerHealthText.text = "Player Health:  " + playerHitPoint.ToString();    
+    }
 
 
     public void TakeDamage(float damage)
     {
         playerHitPoint -= damage;
-        print(playerHitPoint);
         isPlayerDead();
-    
-
     }
+    
     private void isPlayerDead()
     {
         if(playerHitPoint <= 0)
