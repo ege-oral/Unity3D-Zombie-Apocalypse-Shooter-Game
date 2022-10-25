d5354fec3dec6acce2f84ae65d04912ccec94643
Ege Oral <egeoral@gmail.com>
2022-10-25 19:19:06 +0300

    Start menu scene added.

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    private void Awake() 
+    {
+        GameObject[] backgroundMusicObjects = GameObject.FindGameObjectsWithTag("Background Music");
+        if(backgroundMusicObjects.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+
+        if(SceneManager.GetActiveScene().name == "Game")
+        {
+            backgroundMusicObjects[0].GetComponent<AudioSource>().volume = 0.3f;
+        }
+    }
+}
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -2,9 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
+    public GameObject loadingScreen;
+    public Slider slider;
+
+    public void PlayGame()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        StartCoroutine(LoadAsynchronously(currentSceneIndex + 1));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation =  SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            yield return null;
+        }
+
+    }
+
     public void ReloadLevel()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
